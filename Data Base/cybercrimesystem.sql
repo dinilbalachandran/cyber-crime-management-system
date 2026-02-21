5dd6a06d1d59be361823cb1396d6d477c87b2e4e
Dinil Balachandran <dinilbalachandran@gmail.com>
2026-02-21 23:26:07 +0530

    Update cybercrimesystem.sql

--- Data Base/cybercrimesystem.sql
+++ Data Base/cybercrimesystem.sql
@@ -40,10 +40,6 @@ CREATE TABLE `tbl_case` (
 --
 -- Dumping data for table `tbl_case`
 --
-
-INSERT INTO `tbl_case` (`case_id`, `login_id`, `station_id`, `category`, `date`, `time`, `description`) VALUES
-(51, 40, 28, 'x56wfnOpLEH58q/i5M4vgQiZnqDcL6MT6N3gLQ/6RLs=', 'QLRobNGw1VcIWdPaUM2Cbg==', 'pZrcFb9sdft7/P4GTha/3Q==', 'Ihkx&Jklktyk&Yyzks&oy&jkyomtkj&zu&nkrv&otjo|oj{gry&xkvuxz&ihkx&ixosky&w{oiqr&gtj&klloioktzr4Xkvuxzy&gxk&joxkizkj&zu&znk&tkgxkyz&vuroik&yzgzouty2&kty{xotm&vxusvz&gizout4&[ykxy&igt&zxgiq&znk&yzgz{y&ul&znkox&igyky&otxkgr3zosk2&vxu|ojotm&zxgtyvgxkti&gtj&vkgik&ul&sotj4&U{x&mugr&oy&zu&vxu|ojk&g&yki{xk&gtj&yzxkgsrotkj&vxuikyy&lux&ngtjrotmihkx&ixosk&otiojktzy2&iuttkizotm&iozo?kty&joxkizr&}ozn&rg}&ktluxiksktz&gmktioky&jkjoigzkj&zu&iushgzotm&utrotk&znxkgzy4');
-
 -- --------------------------------------------------------
 
 --
@@ -62,9 +58,6 @@ CREATE TABLE `tbl_casestatus` (
 -- Dumping data for table `tbl_casestatus`
 --
 
-INSERT INTO `tbl_casestatus` (`slno`, `case_id`, `station_id`, `filing_date`, `current_status`) VALUES
-(23, 51, 28, '2024-10-11 23:53:34', 'NYbwpauRE/SKK5QrIwmc2A==');
-
 -- --------------------------------------------------------
 
 --
@@ -80,12 +73,6 @@ CREATE TABLE `tbl_district` (
 --
 -- Dumping data for table `tbl_district`
 --
-
-INSERT INTO `tbl_district` (`district_id`, `state_id`, `district_name`) VALUES
-(24, 20, 'pomsg8v0uNJhsuq82Q6QSA=='),
-(25, 24, 'xa1Qn9mdsnIQSEB+QwGlmQ=='),
-(26, 24, '+667zXqVr0jhqORuxINNNA=='),
-(27, 24, 'aKFh64c0uGxVnQ2778kzGA==');
 
 -- --------------------------------------------------------
 
@@ -104,15 +91,6 @@ CREATE TABLE `tbl_login` (
 -- Dumping data for table `tbl_login`
 --
 
-INSERT INTO `tbl_login` (`login_id`, `usertype_id`, `username`, `password`) VALUES
-(20, 3, 'i5spfK2bsgnCtT1L74T25w==', 'aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d'),
-(39, 2, '5BVnISQk5nLmMRZpD0Tmow==', '84a516841ba77a5b4648de2cd0dfcb30ea46dbb4'),
-(40, 3, 'rtrfdny1x5XlmUfBa6tH7Q==', '0d950b17080ee9846b572b451a7c9a40d687c01c'),
-(41, 2, 'bpZNqIAxegJeyq6+Ljx8Yg==', 'd0aaab75800fa4e51005a7fd3181a9caf64bdbb1'),
-(42, 2, 'xqgoCr+5blQt6oleqG+VUw==', 'b220ee2eb110e04999d6aaa434a9d965cd98a2e3'),
-(43, 2, 'oSukC9fVO5HaZ3FSpRwHyA==', 'cef9ae7b75fc545c174d262138b017424b1308c1'),
-(44, 1, 'pA8VZ6qih9OHIKB3i+hEuw==', 'd033e22ae348aeb5660fc2140aec35850c4da997');
-
 -- --------------------------------------------------------
 
 --
@@ -130,12 +108,6 @@ CREATE TABLE `tbl_policestation` (
 --
 -- Dumping data for table `tbl_policestation`
 --
-
-INSERT INTO `tbl_policestation` (`station_id`, `login_id`, `district_id`, `state_id`, `station_name`) VALUES
-(27, 39, 24, 20, 'VrY2SMMUzLzpiDHEzG9V4g=='),
-(28, 41, 25, 24, 'Qd0qqVN1jqdbF/lHj6HTxg=='),
-(29, 42, 25, 24, 'AUVgiooFyY0Wkdkek0/Emw=='),
-(30, 43, 25, 24, '4Lq/dPpsxVJoRu4u4lHMyA==');
 
 -- --------------------------------------------------------
 
@@ -157,10 +129,6 @@ CREATE TABLE `tbl_register` (
 -- Dumping data for table `tbl_register`
 --
 
-INSERT INTO `tbl_register` (`register_id`, `login_id`, `name`, `dob`, `email`, `mobile`, `address`) VALUES
-(20, 20, 'rtrfdny1x5XlmUfBa6tH7Q==', 'D1uDOUMA+OnMcHvw+DHf3w==', '0jtF2BOGc522w1cMcSXfQ48K4wID4cP78WEn83YAxMU=', 'OBC45aeSU0CQygeWlIuZnw==', 'tnM1pCvDI94PrcNKW14zIA=='),
-(21, 40, 'eG5S+5pM2qxt1meVwOUHVi4qZqf6wAkgPIQWT7oZ0so=', 'CgeMRmfsp/J4H6tIHsYvRg==', 'bb0st8In68IB8kSXwCGmgQ==', 'ZuN58iIbDDgr0vlN0d6+OQ==', 'hcManGa3UYw6lLOPsMk0PCNblOARz0ENwsbwXLbDNk4YFXIAdnvCH1MXdYbpCuY3zSwNES8DGuioR7nQO32mhjfQwI9MtwBXKpOTSjT4SgQ=');
-
 -- --------------------------------------------------------
 
 --
@@ -175,11 +143,6 @@ CREATE TABLE `tbl_state` (
 --
 -- Dumping data for table `tbl_state`
 --
-
-INSERT INTO `tbl_state` (`state_id`, `state_name`) VALUES
-(20, '3DAXUDvH0rqJmuui8wUDfw=='),
-(24, 'W9/AlzzcloRWT1lSL3JdNg=='),
-(25, 'CjDeruDB21Qwhkbt8TCisg==');
 
 -- --------------------------------------------------------
 
@@ -198,14 +161,6 @@ CREATE TABLE `tbl_statusupdates` (
 --
 -- Dumping data for table `tbl_statusupdates`
 --
-
-INSERT INTO `tbl_statusupdates` (`slno`, `case_id`, `status`, `timestamp`, `view`) VALUES
-(78, 51, 'FFE7yiziwAasf5EzEUATtw==', '2024-10-11 23:53:34', 1),
-(79, 51, 'XJS+vTrtGoZi1YF3ldmcf1tD7IiV2lQDJmP42+ynawo=', '2024-10-11 23:55:59', 1),
-(80, 51, 'L+BjWVdlW6Lkcj3pgrvfpnoAEnHgzQdSDLHn36sOrKc=', '2024-10-12 00:56:43', 1),
-(81, 51, 't4eZY/njCV37nAGy87/13i8ztguYHIm92MfaDQ2NQ/U=', '2024-10-12 00:57:11', 1),
-(82, 51, 'j4w73+IU5qHe8EHJlw60JP+2iIV/jq88iVsl+A7ET/E=', '2024-10-22 10:09:03', 1),
-(83, 51, 'NYbwpauRE/SKK5QrIwmc2A==', '2024-11-11 10:49:32', 1);
 
 --
 -- Indexes for dumped tables
